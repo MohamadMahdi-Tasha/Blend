dd7d5eeba2b4eeb001fa50fdafea2373f0b67321
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-06-26 11:45:40 +0430

    added bg to last section(not footer) pink bg

--- assets/base/js/swipers.js
+++ assets/base/js/swipers.js
@@ -1,5 +1,5 @@
-// Configuring Swiper Js On Sliders
-const homeFirstSectionSwiper = new Swiper('.dv-home-defualt-swipers', {
+// Default Configs
+const config = {
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
@@ -9,4 +9,8 @@ const homeFirstSectionSwiper = new Swiper('.dv-home-defualt-swipers', {
         prevEl: '.swiper-button-prev',
         nextEl: '.swiper-button-next',
     }
-})
+}
+
+// Configuring Swiper Js On Sliders
+const homeFirstSectionSwiper = new Swiper('.dv-home-defualt-swipers', config)
+const homeFirstSectionSwiperWithSpace = new Swiper('.dv-home-defualt-swipers-with-space', {spaceBetween: 100, config})
