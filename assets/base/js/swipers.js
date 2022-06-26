d725c204b2f2ff806853abd7569a5f1d21af0511
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-06-26 17:04:40 +0430

    added projects page

--- assets/base/js/swipers.js
+++ assets/base/js/swipers.js
@@ -14,3 +14,13 @@ const config = {
 // Configuring Swiper Js On Sliders
 const homeFirstSectionSwiper = new Swiper('.dv-home-defualt-swipers', config)
 const homeFirstSectionSwiperWithSpace = new Swiper('.dv-home-defualt-swipers-with-space', {spaceBetween: 100, config})
+const projectsSwiperSlide = new Swiper('.dv-projects-swiper-slide', {
+    spaceBetween: 10,
+    slidesPerView:4,
+    breakpoints: {
+        991: {slidesPerView: 4,},
+        575: {slidesPerView: 2,},
+        1:{slidesPerView: 1,},
+    },
+    config
+})
