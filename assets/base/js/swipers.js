1473a1788069bdacf9fc4e2249bf2799cb8908db
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-07-12 21:52:53 +0430

    added mouse control to slider

--- assets/base/js/swipers.js
+++ assets/base/js/swipers.js
@@ -1,5 +1,5 @@
 // Configuring Swiper Js On Sliders
-const homeFirstSectionSwiper = new Swiper('.dv-home-defualt-swipers', {
+const config = {
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
@@ -8,30 +8,14 @@ const homeFirstSectionSwiper = new Swiper('.dv-home-defualt-swipers', {
         prevEl: '.swiper-button-prev',
         nextEl: '.swiper-button-next',
     }
-})
+}
 
-const homeFirstSectionSwiperWithSpace = new Swiper('.dv-home-defualt-swipers-with-space', {
-    spaceBetween: 10,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-    },
-    navigation: {
-        prevEl: '.swiper-button-prev',
-        nextEl: '.swiper-button-next',
-    }
-})
-
+const categoryPagesSlider = new Swiper('.dv-category-page-slider', {mousewheel: true, config,})
+const homeFirstSectionSwiperWithSpace = new Swiper('.dv-home-defualt-swipers-with-space', {spaceBetween: 10, config,})
+const homeFirstSectionSwiper = new Swiper('.dv-home-defualt-swipers', config)
 const sliderWith4Slides = new Swiper('.dv-slider-4-slide', {
     slidesPerView: 4,
     spaceBetween: 10,
     breakpoints: {991: {slidesPerView: 4,}, 767: {slidesPerView: 3,}, 575: {slidesPerView: 2,}, 1: {slidesPerView: 1,},},
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-    },
-    navigation: {
-        prevEl: '.swiper-button-prev',
-        nextEl: '.swiper-button-next',
-    }
+    config
 })
